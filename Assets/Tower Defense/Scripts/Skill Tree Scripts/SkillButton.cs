108033ca700ccfa1c3912027ae78cdff0d1218b2
sakuraema <tonychu122003@gmail.com>
2021-03-18 00:50:28 +0900

    Skill tree system

---
+++ Assets/Tower Defense/Scripts/Skill Tree Scripts/SkillButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillButton : MonoBehaviour
+{
+    public Image skillImage;
+    public Text skillNameText;
+    public Text skillDesText;
+
+    public int skillButtonId; // Each Button has one unique Button id correspond with the same order as the Skill array
+
+    // This method will be called when we press each skill
+    public void PressSkillButton()
+    {
+        SkillManager.instance.activateSkill = transform.GetComponent<Skill>();
+
+        skillImage.sprite = SkillManager.instance.skills[skillButtonId].skillSprite;
+        skillNameText.text = SkillManager.instance.skills[skillButtonId].skillName;
+        skillDesText.text = SkillManager.instance.skills[skillButtonId].skillDes;
+    }
+}
