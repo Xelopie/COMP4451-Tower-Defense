eeae0dfd3c4c8beea400c9ad2ba52a5fbc3ccac5
sakuraema <tonychu122003@gmail.com>
2021-03-20 00:02:02 +0900

    Skill tree, Lv up UI

---
+++ Assets/Tower Defense/Scripts/Skill Tree Scripts/LvUpButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LvUpButton : MonoBehaviour
+{
+    // This method will be called when we press Lv button
+    public void OnPressLvButton()
+    {
+        // TODO Call the lv up UI scene
+    }
+}
