4c545f501ba218146697406bad5fa9e16e18d7b3
Xelopie <47631876+Xelopie@users.noreply.github.com>
2021-03-28 01:55:53 +0800

    Import required assets for healer class

---
+++ Assets/Tower Defense/Scripts/HealerAttackAffector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TowerDefense.Affectors;
+using UnityEngine;
+
+public class HealerAttackAffector : AttackAffector
+{
+	[Header("Customized attributes for Healer")]
+	public Animator healerAnimator;
+
+	protected override void OnFireTimer()
+	{
+		if (fireCondition != null)
+		{
+			if (!fireCondition())
+			{
+				return;
+			}
+		}
+
+		if (m_TrackingEnemy != null)
+		{
+			FireProjectile();
+			healerAnimator.SetTrigger("Single Heal");
+		}
+	}
+}
