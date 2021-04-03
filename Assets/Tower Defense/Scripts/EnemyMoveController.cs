8aab5aacf6baaa5825935e2142559fb94a44a879
Xelopie <47631876+Xelopie@users.noreply.github.com>
2021-04-03 23:39:07 +0800

    Setup multiple Boar enemies with prefab variant

--- Assets/Tower Defense/Scripts/EnemyMoveController.cs
+++ Assets/Tower Defense/Scripts/EnemyMoveController.cs
@@ -1,4 +1,4 @@
-﻿using Core.Health;
+using Core.Health;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -9,6 +9,18 @@ public class EnemyMoveController : MonoBehaviour
 
     public bool moveAnimation = true;
     public bool dieAnimation = true;
+	public bool attackAnimation = true;
+
+	// Return true if the attack animation can be triggered
+	public bool TriggerAttack()
+	{
+		if (attackAnimation && !enemyAnimator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+		{
+			enemyAnimator.SetTrigger("Attack");
+			return true;
+		}
+		return false;
+	}
 
     private void OnDied(DamageableBehaviour db)
     {
